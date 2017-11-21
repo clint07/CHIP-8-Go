012fddbfabd649705562e87126060fdc69b5cfe5
Clint Adderly Ascencio <clintascencio+github@gmail.com>
2017-11-20 18:54:40 -0600

    Add opcode instruction skeleton and documentation

--- chip8/chip8.go
+++ chip8/chip8.go
@@ -75,12 +75,12 @@ func (chip8 *CHIP8) Cycle() {
 }
 
 func (chip8 *CHIP8) execute(opCode uint16) error {
-	vx := opCode & 0x0F00
-	vy := opCode & 0x00F0
+	vx := byte(opCode & 0x0F00)
+	vy := byte(opCode & 0x00F0)
 
-	nnn := opCode & 0x0FFF
-	kk := opCode & 0x00FF
-	n := opCode & 0x000F
+	nnn := uint16(opCode & 0x0FFF)
+	kk := byte(opCode & 0x00FF)
+	n := byte(opCode & 0x000F)
 
 	if opCode == 0x00E0 {
 		fmt.Println("Instruction 00E0: Clear the display.")
@@ -92,7 +92,7 @@ func (chip8 *CHIP8) execute(opCode uint16) error {
 
 	} else if (opCode & 0xF000) == 0x1000 {
 		fmt.Println("Instruction 1nnn: Jump to location nnn.")
-		chip8.jmp(nnn)
+		chip8.jump(nnn)
 
 	} else if (opCode & 0xF000) == 0x2000 {
 		fmt.Println("Instruction 2nnn: Call subroutine at nnn.")
@@ -222,6 +222,241 @@ func (chip8 *CHIP8) execute(opCode uint16) error {
 	} else {
 		fmt.Errorf("Unknown instruction: %X\n\n", opCode)
 	}
-
 	return nil
+}
+
+// Instruction 00E0: Clear the display.
+func (chip8 *CHIP8) clear() {
+
+}
+
+// Instruction 00EE: Return from a subroutine.
+// The interpreter sets the program counter to the address at the top of the stack,
+// then subtracts 1 from the stack pointer.
+func (chip8 *CHIP8) ret() {
+
+}
+
+// Instruction 1nnn: Jump to location nnn.
+// The interpreter sets the program counter to nnn.
+func (chip8 *CHIP8) jump(nnn uint16) {
+
+}
+
+// Instruction 2nnn: Call subroutine at nnn.
+// The interpreter increments the stack pointer, then puts the current PC on the top of the stack.
+// The PC is then set to nnn.
+func (chip8 *CHIP8) call(nnn uint16) {
+
+}
+
+// Instruction 3xkk: Skip next instruction if Vx = kk.
+// The interpreter compares register Vx to kk, and if they are equal,
+// increments the program counter by 2.
+func (chip8 *CHIP8) skipIf(vx byte, kk byte) {
+
+}
+
+// Instruction 4xkk: Skip next instruction if Vx != kk.
+// The interpreter compares register Vx to kk, and if they are not equal,
+// increments the program counter by 2.
+func (chip8 *CHIP8) skipIfNot(vx byte, kk byte) {
+
+}
+
+// Instruction 5xy0: Skip next instruction if Vx = Vy.
+// The interpreter compares register Vx to register Vy, and if they are equal,
+// increments the program counter by 2.
+func (chip8 *CHIP8) skipIfXY(vx byte, vy byte) {
+
+}
+
+// Instruction 6xkk: Set Vx = kk.
+// The interpreter puts the value kk into register Vx.
+func (chip8 *CHIP8) load(vx byte, kk byte) {
+
+}
+
+// Instruction 7xkk: Set Vx = Vx + kk.
+// Adds the value kk to the value of register Vx, then stores the result in Vx.
+func (chip8 *CHIP8) add(vx byte, kk byte) {
+
+}
+
+// Instruction 8xy0: Set Vx = Vy.
+// Stores the value of register Vy in register Vx.
+func (chip8 *CHIP8) loadXY(vx byte, vy byte) {
+
+}
+
+// Instruction 8xy1: Set Vx = Vx OR Vy.
+// Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx.
+// A bitwise OR compares the corrseponding bits from two values, and if either bit is 1,
+// then the same bit in the result is also 1. Otherwise, it is 0.
+func (chip8 *CHIP8) orXY(vx byte, vy byte) {
+
+}
+
+// Instruction 8xy2: Set Vx = Vx AND Vy.
+// Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx.
+// A bitwise AND compares the corrseponding bits from two values, and if both bits are 1,
+// then the same bit in the result is also 1. Otherwise, it is 0.
+func (chip8 *CHIP8) andXY(vx byte, vy byte) {
+
+}
+
+// Instruction 8xy3: Set Vx = Vx XOR Vy.
+// Performs a bitwise exclusive OR on the values of Vx and Vy, then stores the result in Vx.
+// An exclusive OR compares the corrseponding bits from two values,
+// and if the bits are not both the same, then the corresponding bit in the result is set to 1.
+// Otherwise, it is 0.
+func (chip8 *CHIP8) xorXY(vx byte, vy byte) {
+
+}
+
+// Instruction 8xy4: Set Vx = Vx + Vy, set VF = carry.
+// The values of Vx and Vy are added together. If the result is greater than 8 bits (i.e., > 255,)
+// VF is set to 1, otherwise 0. Only the lowest 8 bits of the result are kept, and stored in Vx.
+func (chip8 *CHIP8) addXY(vx byte, vy byte) {
+
+}
+
+// Instruction 8xy5: Set Vx = Vx - Vy, set VF = NOT borrow.
+// If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx,
+// and the results stored in Vx.
+func (chip8 *CHIP8) subXY(vx byte, vy byte) {
+
+}
+
+// Instruction 8xy6: Set Vx = Vx SHR 1.
+// If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0.
+// Then Vx is divided by 2.
+func (chip8 *CHIP8) shiftRight(vx byte) {
+
+}
+
+// Instruction 8xy7: Set Vx = Vy - Vx, set VF = NOT borrow.
+// If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy,
+// and the results stored in Vx.
+func (chip8 *CHIP8) subnXY(vx byte, vy byte) {
+
+}
+
+// Instruction 8xyE: Set Vx = Vx SHL 1.
+// If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0.
+// Then Vx is multiplied by 2.
+func (chip8 *CHIP8) shiftLeft(vx byte) {
+
+}
+
+// Instruction 9xy0: Skip next instruction if Vx != Vy.
+// The values of Vx and Vy are compared, and if they are not equal,
+// the program counter is increased by 2.
+func (chip8 *CHIP8) skipIfNotXY(vx byte, vy byte) {
+
+}
+
+// Instruction Annn: Set I = nnn.
+// The value of register I is set to nnn.
+func (chip8 *CHIP8) loadI(nnn uint16) {
+
+}
+
+// Instruction Bnnn: Jump to location nnn + V0.
+// The program counter is set to nnn plus the value of V0.
+func (chip8 *CHIP8) jumpV0(nnn uint16) {
+
+}
+
+// Instruction Cxkk: Set Vx = random byte AND kk.
+// The interpreter generates a random number from 0 to 255,
+// which is then ANDed with the value kk. The results are stored in Vx.
+// See instruction 8xy2 for more information on AND.
+func (chip8 *CHIP8) rand(vx byte) {
+
+}
+
+// Instruction Dxyn: Display n-byte sprite starting at memory location I at (Vx, Vy),
+// set VF = collision.
+//
+// The interpreter reads n bytes from memory, starting at the address stored in I.
+// These bytes are then displayed as sprites on screen at coordinates (Vx, Vy).
+// Sprites are XORed onto the existing screen. If this causes any pixels to be erased,
+// VF is set to 1, otherwise it is set to 0. If the sprite is positioned so part of it
+// is outside the coordinates of the display, it wraps around to the opposite side of the screen.
+// See instruction 8xy3 for more information on XOR, and section 2.4, Display,
+// for more information on the Chip-8 screen and sprites.
+func (chip8 *CHIP8) draw(vx byte, vy byte, n byte) {
+
+}
+
+// Instruction Ex9E: Skip next instruction if key with the value of Vx is pressed.
+// Checks the keyboard, and if the key corresponding to the value of Vx is currently
+// in the down position, PC is increased by 2.
+func (chip8 *CHIP8) skipIfKey(vx byte) {
+
+}
+
+// Instruction ExA1: Skip next instruction if key with the value of Vx is not pressed.
+// Checks the keyboard, and if the key corresponding to the value of Vx is currently
+// in the up position, PC is increased by 2.
+func (chip8 *CHIP8) skipIfKeyNot(vx byte) {
+
+}
+
+// Instruction Fx07: Set Vx = delay timer value.
+// The value of DT is placed into Vx.
+func (chip8 *CHIP8) loadXDT(vx byte) {
+
+}
+
+// Instruction Fx0A: Wait for a key press, store the value of the key in Vx.
+// All execution stops until a key is pressed, then the value of that key is stored in Vx.
+func (chip8 *CHIP8) loadKey(vx byte) {
+
+}
+
+// Instruction Fx15: Set delay timer = Vx.
+// DT is set equal to the value of Vx.
+func (chip8 *CHIP8) loadDTX(vx byte) {
+
+}
+
+// Instruction Fx18: Set sound timer = Vx.
+// ST is set equal to the value of Vx.
+func (chip8 *CHIP8) loadSTX(vx byte) {
+
+}
+
+// Instruction Fx1E: Set I = I + Vx.
+// The values of I and Vx are added, and the results are stored in I.
+func (chip8 *CHIP8) addIX(vx byte) {
+
+}
+
+// Instruction Fx29: Set I = location of sprite for digit Vx.
+// The value of I is set to the location for the hexadecimal sprite corresponding
+// to the value of Vx. See section 2.4, Display, for more information on the Chip-8 hexadecimal font.
+func (chip8 *CHIP8) loadIX(vx byte) {
+
+}
+
+// Instruction Fx33: Store BCD representation of Vx in memory locations I, I+1, and I+2.
+// The interpreter takes the decimal value of Vx, and places the hundreds digit in memory
+// at location in I, the tens digit at location I+1, and the ones digit at location I+2.
+func (chip8 *CHIP8) loadBCD(vx byte) {
+
+}
+
+// Instruction Fx55: Store registers V0 through Vx in memory starting at location I.
+// The interpreter copies the values of registers V0 through Vx into memory,
+// starting at the address in I.
+func (chip8 *CHIP8) saveV(vx byte) {
+
+}
+
+// Instruction Fx65: Read registers V0 through Vx from memory starting at location I.
+// The interpreter reads values from memory starting at location I into registers V0 through Vx.
+func (chip8 *CHIP8) loadV(vx byte) {
+
 }
